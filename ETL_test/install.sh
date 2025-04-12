bc26c213c79599f325b4ff7f07230c6aa4908fb6
OlegJr <olegjr@mail.ru>
2025-04-12 20:18:00 +0300

    1

---
+++ ETL_test/install.sh
@@ -0,0 +1,6 @@
+apt-get install mc python3-full python3-pip python3-pandas python3-psycopg2 python3-xlsxwriter python3-python-telegram-bot -y
+python3 -m venv ./venv
+./venv/bin/pip install telebot
+cp ./venv/lib/python3.11/site-packages/* /usr/lib/python3/dist-packages/ -r -n
+
+begin.sh
