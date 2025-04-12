18a3052a823515b9020269abf3411be9ac84c54b
OlegJr <olegjr@mail.ru>
2025-04-12 23:04:29 +0300

    1

--- install.sh
+++ install.sh
@@ -1,5 +1,5 @@
 #!/usr/bin/bash
-apt-get install mc python3-full python3-pip python3-pandas python3-psycopg2 python3-xlsxwriter python3-python-telegram-bot htop -y
+apt-get install mc python3-full python3-pip python3-pandas python3-psycopg2 python3-xlsxwriter htop -y
 python3 -m venv ./venv
 ./venv/bin/pip install telebot
 cp ./venv/lib/python3.11/site-packages/* /usr/lib/python3/dist-packages/ -r -n
